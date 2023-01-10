88f339ab3ce8ab881deaef07d69212a795090332
Reed <reed.cannon97@gmail.com>
2023-01-09 17:14:11 -0700

    Finished assigment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string percentageString;
+        int percentage;
+        int gradeA = 90;
+        int gradeB = 80;
+        int gradeC = 70;
+        int gradeD = 60;
+
+        Console.WriteLine("Please enter grade percentage: ");
+        percentageString = Console.ReadLine();
+        percentage = int.Parse(percentageString);
+
+        if (percentage >= gradeA)
+        {
+            Console.WriteLine("You recieved an A.");
+        }
+
+        else if (percentage >= gradeB)
+        {
+            Console.WriteLine("You recieved a B.");
+        }
+
+        else if (percentage >= gradeC)
+        {
+            Console.WriteLine("You recieved a C.");
+        }
+
+        else if (percentage >= gradeD)
+        {
+            Console.WriteLine("You recieved a D.");
+        }
+
+        else
+        {
+            Console.WriteLine("You recieved an F.");
+        }
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random random = new Random();
+ 
+        string GuessNumberInputString;
+        int GuessNumber;
+        bool KeepPlaying = true;
+        string KeepPlayingInput;
+
+
+        while (KeepPlaying)
+        
+        {
+            int RandomMagicNumber = random.Next(1, 101);
+            int NumberOfGuesses = 0;
+            bool GuessedCorrectly = false;
+            while (GuessedCorrectly == false)
+
+            {
+                Console.WriteLine("What is your guess? ");
+
+                GuessNumberInputString = Console.ReadLine();
+
+                GuessNumber = Int32.Parse(GuessNumberInputString);
+
+
+                if (GuessNumber > RandomMagicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    NumberOfGuesses += 1;
+                    Console.WriteLine($"Guesses: {NumberOfGuesses}");
+                }
+
+                else if (GuessNumber < RandomMagicNumber)
+                {
+                    Console.WriteLine("Higher");
+                    NumberOfGuesses += 1;
+                    Console.WriteLine($"Guesses: {NumberOfGuesses}");
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    NumberOfGuesses += 1;
+                    Console.WriteLine($"Guesses: {NumberOfGuesses}");
+                    GuessedCorrectly = true;
+                    Console.WriteLine("Would you like to play again? (y/n)");
+                    KeepPlayingInput = Console.ReadLine();
+
+                    if (KeepPlayingInput != "y")
+                    {
+                        KeepPlaying = false;
+                    }
+                    
+                }
+            }
+        }
+
     }
 }
